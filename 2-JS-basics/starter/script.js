1227522780f579d6476dfa443e9ec89d0e679508
Humberto Hernandez <hamburger45@gmail.com>
2019-09-30 20:33:43 -0700

    Coding Challenge 1

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -92,39 +92,70 @@ console.log(firstName + ' ' + lastName);
  */
 
 
-var now = 2018;
-var yearJohn = 1989;
-var fullAge = 18;
+// var now = 2018;
+// var yearJohn = 1989;
+// var fullAge = 18;
 
-// Multiple operators
-var isFullAge = now - yearJohn >= fullAge; // true
-console.log(isFullAge);
+// // Multiple operators
+// var isFullAge = now - yearJohn >= fullAge; // true
+// console.log(isFullAge);
 
-// Grouping
-var ageJohn = now - yearJohn;
-var ageMark = 35;
-var average = (ageJohn + ageMark) / 2;
-console.log(average);
+// // Grouping
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+// var average = (ageJohn + ageMark) / 2;
+// console.log(average);
 
-// Multiple assignments
-var x, y;
-x = y = (3 + 5) * 4 -6;// 8 * 4 -  6 // 32 - 6 // 26
-console.log(x, y);
+// // Multiple assignments
+// var x, y;
+// x = y = (3 + 5) * 4 -6;// 8 * 4 -  6 // 32 - 6 // 26
+// console.log(x, y);
 
-// More operartors
-x *= 2;
-console.log(x);
-x += 10;
-console.log(x);
- x = x + 1;
- console.log(x);
- x += 1;
- console.log(x);
- x++;
- console.log(x);
+// // More operartors
+// x *= 2;
+// console.log(x);
+// x += 10;
+// console.log(x);
+//  x = x + 1;
+//  console.log(x);
+//  x += 1;
+//  console.log(x);
+//  x++;
+//  console.log(x);
 
+/***************************
+ * CODING CHALLENGE 1
+ */
 
+ /*
+  Mark and John are trying to compare their BMI (Body
+  Mass Index), which is calculated using the formula:
+  BMI = mass / height^2 = mass / (height * height).
+  (mass in kg and height in meter).
 
+  1. Store Mark's andf John's mass and height in
+  variables
+  2. Calculate both their BMIs
+  3. Create a boolean variable containing information
+  about whether Mark has a higher BMI than John.
+  4. Print a string to the console containing 
+  the variable from Step 3. (Something like "Is mark's BMI
+  higher than John's? true").
+
+ */
+
+ var markHeight, johnHeight, markMass, johnMass;
+ markHeight = 1.8288;
+ johnHeight = 2.01168;
+ markMass = 90.7185;
+ johnMass = 86.1826;
+
+ var markBMI = markMass / (markHeight * markHeight);
+ var johnBMI = johnMass / (johnHeight * johnHeight);
+console.log(markBMI, johnBMI);
+
+ var greaterBMI = markBMI > johnBMI;
+console.log(`Is Mark's BMI higher than John's? ${greaterBMI}`);
 
 
 
