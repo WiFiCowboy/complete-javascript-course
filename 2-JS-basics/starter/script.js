89fbc47be65f3fcb520b23ff540d2c8cc5948fbc
Humberto Hernandez <hamburger45@gmail.com>
2019-09-30 18:12:58 -0700

    Basic Operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -28,6 +28,7 @@ var if = 23;
  * Varible mutation and type coercion
  */
 
+ /***************************** 
 var firstName = 'John';
 var age = 28;
 
@@ -48,15 +49,40 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+*/
 
 
 
+/*****************************
+ * Basic operators
+ */
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+// Math operators
+yearJohn = now - 28;
+yearMark = now - 33;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
 
 
 
-
-
-
+// Typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageMark);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
 
 
 
