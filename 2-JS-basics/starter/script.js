18e9c50234cd14a45d4060540c6aca28d585596d
Humberto Hernandez <hamburger45@gmail.com>
2019-10-09 19:15:00 -0700

    challenge 5

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -555,22 +555,139 @@ In the end, John would like to have 2 arrays:
  * Loops and iteration
  */
 
+// //  For Loop
+// for (var i = 1; i <= 20 ; i += 2) {
+//   console.log(i);
+// }
 
+// // i = 0, 0 < 10 true, log i to console, i++
+// // i = 1, 1 < 10 true, log i to console, i++
+// // ...
+// // i = 9, 9 < 10 true, log i to console, i++
+// // i = 10, 10 < 10 false, log i to console, i++
+
+
+// var john = ['John', 'Smith', 1990, 'designer', false];
+
+// for (var i = 0; i < john.length; i++){
+//   console.log(john[i]);
+// }
+
+// // While loop
+// var i = 0;
+// while (i < john.length){
+//   console.log(john[i]);
+//   i++
+// }
+
+// Continue and break statements
+
+// var john = ['John', 'Smith', 1990, 'designer', false];
+
+// for (var i = 0; i < john.length; i++){
+//   if (typeof john[i] !== 'string') continue;
+//   console.log(john[i]);
+// }
+
+// for (var i = 0; i < john.length; i++){
+//   if (typeof john[i] !== 'string') break;
+//   console.log(john[i]);
+// }
+
+
+// // Looping backwards
+// for (var i = john.length - 1; i >= 0; i--){
+//   console.log(john[i]);
+// }
+
+/***************************
+ * Coding Challenge 5
+ */
 
  
+var john = {
+  fullName: 'John Smith',
+  bills: [124, 48, 268, 180, 42],
+  calcTips: function(){
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      //  Determine % based on tipping rules
+      var percentage;
+      var bill = this.bills[i];
+
+      if (bill < 50) {
+        percentage = .20;
+      } else if(bill >= 50 && bill < 200) {
+        percentage = .15;
+      } else{
+        percentage = .1;
+      }
+
+
+      //  Add results to the corresponing arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+
+    }
+  }
+};
 
 
 
+var mark = {
+  fullName: 'Mark Miller',
+  bills: [77, 475, 110, 45],
+  calcTips: function(){
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      //  Determine % based on tipping rules
+      var percentage;
+      var bill = this.bills[i];
+
+      if (bill < 100) {
+        percentage = .20;
+      } else if(bill >= 100 && bill < 300) {
+        percentage = .1;
+      } else{
+        percentage = .25;
+      }
 
 
+      //  Add results to the corresponing arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+
+    }
+  }
+};
 
 
+function calcAverage(tips) {
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++) {
+    sum = sum + tips[i];
+  }
+  return sum / tips.length;
+}
 
 
+//  Do the calculations
+john.calcTips();
+mark.calcTips();
 
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
 
-
-
+if (john.average > mark.average){
+  console.log(`${john.fullName}'s family pays higher tips, with an average of $${john.average}`);
+} else {
+  console.log(`${mark.fullName}'s family pays higher tips, with an average of $${mark.average}`);
+} 
 
 
 
